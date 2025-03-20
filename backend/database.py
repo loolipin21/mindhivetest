dec8d9b8c38dd50adaaa7169abb98c6f5e22583a
lloo0007 <lloo0007@student.monash.edu>
2025-03-20 18:20:21 +0800

    Database modification

--- backend/database.py
+++ backend/database.py
@@ -8,7 +8,7 @@ DB_CONFIG = {
     "password": os.getenv("DB_PASSWORD"),
     "host": os.getenv("DB_HOST"),
     "port": os.getenv("DB_PORT"),
-    "sslmode": "required"
+    "sslmode": "prefer"
 }
 
 def get_db_connection():
